573e6cccdaeb6c96446862437160233d31f0089c
Giancarlo Torres <immortalg23@gmail.com>
2021-06-16 00:46:20 -0700

    "JS needs debug"

---
+++ script.js
@@ -0,0 +1,43 @@
+const question = document.querySelector("#question");
+const scoreText = document.querySelector("#score");
+
+let score = 0
+
+let questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        choice1: "strings",
+        choice2: "booleans",
+        choice3: "alerts",
+        choice4: "numbers",
+        answer: 3,
+    },
+    {
+        question: "The condition in an if / else statement is enclosed within _______.",
+        choice1: "quotes",
+        choice2: "curly brackets",
+        choice3: "parenthesis",
+        choice4: "square brackets",
+        answer: 3,
+    },
+    {
+        question: "Arrays in JavaScript can be used to store _______.",
+        choice1: "numbers and strings",
+        choice2: "other arrays",
+        choice3: "booleans",
+        choice4: "all of the above",
+        answer: 4,
+    },
+    {
+        question: "String values must be enclosed within ______ when being assigned to variables.",
+        choice1: "commas",
+        choice2: "curly brackets",
+        choice3: "quotes",
+        choice4: "parenthesis",
+        answer: 3,
+    },
+]
+
+
+
+document.getElementById("")("click", myScript);
